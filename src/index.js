13b291cf018e389688a0408c2acb6eb62946b751
vm-mishchenko <vm.mishchenko@gmail.com>
2021-04-13 23:21:19 -0700

    Create a simple HTTP server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  res.end(`{"message": "This is a JSON response"}`);
+});
+
+server.listen(5000);
+console.log('Node.js web server at port 5000 is running..')
