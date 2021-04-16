8a365e1ce068f7ff5a30c7232a7b7110426e5e27
Vitalii Mishchenko <31067836+vm-mishchenko@users.noreply.github.com>
2021-04-16 14:39:59 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer(function (req, res) {
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
-  res.end(`{"message": "This is a JSON response5"}`);
+  res.end(`{"message": "This is a JSON response6"}`);
 });
 
 server.listen(5000);
